f104c42f618aeba27005ff28baecfe0ee4b8a6d2
RafaelNatsu <rafaelnatsu99@gmail.com>
2023-10-07 17:36:38 -0300

    feat: Cria base de dados e baixar bulas.
    Foi implementado as estratégias para solucionar o problema da instabilidade no sistema da ANVISA.
    Executando o index.py é criado a base de dados com os medicamentos cadastrados no bulario.
    Executando o baixarBula.py é iniciado o processo para baixar as bulas. Vale ressaltar que DEVE ser executado o script até que tenha obtido todos as bulas (em torno de umas 8500).

---
+++ src/baixarBula.py
@@ -0,0 +1,97 @@
+import concurrent.futures
+import json
+import logging
+import requests
+import re
+import time
+from bulario.bulario import Bulario
+from utils.salvarPdf import salvarPdf
+from database.criaBanco import criarBanco
+from database.updateBanco import updateMedicamento
+from database.selectBanco import selectTodosSemBula, selectQuantidadeRegistros
+
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def criarBancoEIniciar():
+    criarBanco()
+
+def realizarRequisicao(idMedicamento):
+    try:
+        bula = Bulario()
+        # Constroi a url
+        url = bula.pesquisaPorIdMedicamento(idMedicamento)
+        resposta = requests.get(url, headers=bula.getHeader())
+
+        # Retorna json
+        if resposta.status_code == 200:
+            jsonResponseProximo = json.loads(resposta.text)
+            jsonResponseProximo = jsonResponseProximo.get('content')[0]
+            return jsonResponseProximo
+        elif resposta.status_code == 429:
+            time.sleep(10)
+            return None
+        else:
+            print(f"Requisição do registro medicamento: {idMedicamento} falhou. Code:{resposta.status_code} Tentando novamente...")
+            return None
+    except Exception as e:
+        logging.error(f"Erro ao processar a requisição do registro medicamento {idMedicamento}: {str(e)}")
+        return None
+
+def realizarRequisicaoDownload(idBulaPacienteProtegido):
+    try:
+        bula = Bulario()
+        urlProximo = bula.criaUrlPdf(idBulaPacienteProtegido)
+        resposta = requests.get(urlProximo, headers=bula.getHeader())
+        if resposta.status_code == 200:
+            return resposta
+        elif resposta.status_code == 429:
+            time.sleep(10)
+            return None
+        else:
+            print(f"Requisição do registro medicamento: {idBulaPacienteProtegido} falhou. Tentando novamente...")
+            return None
+    except Exception as e:
+        print(f"Erro ao processar a requisição do registro medicamento {idBulaPacienteProtegido}: {str(e)}")
+        return None
+
+def processarMedicamento(idMedicamento):
+    try:
+        bula = Bulario()
+        registroMedicamento = realizarRequisicao(idMedicamento);
+        # verifica se existe algo
+        if registroMedicamento is not None:
+            pdf = realizarRequisicaoDownload(registroMedicamento.get('idBulaPacienteProtegido'))
+            if isinstance(pdf, requests.Response):
+                #
+                contentDispositionHeader = pdf.headers['Content-Disposition']
+                filename = re.findall("filename=(.+)", contentDispositionHeader)
+                nomeArquivo = filename[0].strip('"')
+                salvarPdf("./pdf/"+nomeArquivo, pdf.content)
+                updateMedicamento(idMedicamento, 'S')
+            else:
+                return {"erro_no_id": idMedicamento}
+        else:
+            return {"erro_no_id": idMedicamento}
+    except Exception as e:
+        print(f"Erro ao processar da requisição do registro medicamento {idMedicamento}: {str(e)}")
+
+def main():
+
+    #Verifica caso existe, se não é criado.
+    criarBancoEIniciar()
+
+    #resgata do banco todos registro com o existe_bula = 'N'
+    registrosMedicamentos = selectTodosSemBula()
+    
+    tamanhoGrupo = 4
+    # inicia o loop do processamento
+    with concurrent.futures.ThreadPoolExecutor(max_workers=tamanhoGrupo) as executor:
+        for idMedicamento in registrosMedicamentos:
+            executor.submit(processarMedicamento, idMedicamento)
+        time.sleep(10)
+
+    print("Todas as requisições foram processadas. Execute novamente caso esteja faltando")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/index.py
@@ -0,0 +1,65 @@
+import threading
+import time
+import json
+import logging
+import concurrent.futures
+import requests
+from bulario.bulario import Bulario
+from database.criaBanco import criarBanco
+from database.insertBanco import inserirMedicamento
+
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# tenta executar a requisição e espera o codigo 200, caso não de, ele tenta novamente 3 vezes
+def realizarRequisicao(nomeMedicamento, pagina, quantidade):
+    tentativas = 0
+    while tentativas < 3:
+        try:
+            bula = Bulario()
+            urlProximo = bula.pesquisar(nomeMedicamento=nomeMedicamento, quantidade=quantidade, pagina=pagina)
+            response = requests.get(urlProximo, headers=bula.getHeader())
+            if response.status_code == 200:
+                jsonResponseProximo = json.loads(response.text)
+                return jsonResponseProximo
+            else:
+                logging.error(f"Requisição {nomeMedicamento} falhou. Tentando novamente...")
+        except Exception as e:
+            logging.error(f"Erro ao processar a requisição {nomeMedicamento}: {str(e)}")
+        tentativas += 1
+        time.sleep(60 * 2)
+
+    logging.error(f"Requisição {nomeMedicamento} falhou após {tentativas} tentativas.")
+
+def insereRemedioBanco(nomeMedicamento, pagina, quantidade):
+    resultado = realizarRequisicao(nomeMedicamento, pagina, quantidade)
+    count = 0
+    for medicamento in resultado.get('content'):
+        inserirMedicamento(medicamento['nomeProduto'], medicamento['numeroRegistro'], 'N')
+        # faz uma pausa para evitar problemas no sqlite por quantidade de registro por transação.
+        if count % 500 == 0:
+            time.sleep(0.5)
+        count += 1
+
+def main():
+    # Verifica se existe o banco
+    criarBanco()
+    #faz uma requisição para resgatar a quantidade de paginas e alguns medicamentos
+    jsonResponse = realizarRequisicao("", pagina=1, quantidade=1000)
+    quantidadeTotalDePaginas = jsonResponse.get('totalPages')
+    tamanhoGrupo = 4
+
+    # processa de 4 em 4
+    with concurrent.futures.ThreadPoolExecutor(max_workers=tamanhoGrupo) as executor:
+        for i in range(1, quantidadeTotalDePaginas + 1, tamanhoGrupo):
+            grupoThreads = []
+            for j in range(tamanhoGrupo):
+                pag = i + j
+                if pag <= quantidadeTotalDePaginas:
+                    t = executor.submit(insereRemedioBanco, "", pag, 1000)
+                    grupoThreads.append(t)
+            for t in grupoThreads:
+                t.result()  # Aguarda o término da thread
+    print("Todas as requisições foram processadas.")
+
+if __name__ == "__main__":
+    main()
